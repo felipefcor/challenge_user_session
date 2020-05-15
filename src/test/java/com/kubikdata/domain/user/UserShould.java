f2da8f60e87db64e10b489bd26f471d8a7e6cbbb
Felipe <felipefcor@gmail.com>
2020-05-15 08:09:21 +0200

    feat: exception when checks the token in user and is wrong

--- src/test/java/com/kubikdata/domain/user/UserShould.java
+++ src/test/java/com/kubikdata/domain/user/UserShould.java
@@ -1,9 +1,12 @@
 package com.kubikdata.domain.user;
 
 import com.kubikdata.domain.User;
+import com.kubikdata.domain.valueObjects.IncorrectTokenException;
 import com.kubikdata.domain.valueObjects.UserToken;
 import com.kubikdata.domain.valueObjects.Username;
+import com.kubikdata.domain.valueObjects.UsernameNotValidException;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -16,9 +19,15 @@ public class UserShould {
 
     @Test
     public void check_if_has_the_rigth_token(){
-
         UserToken userToken = new UserToken("token");
 
         Assert.assertEquals(user, user.checkToken(userToken));
+    }
+
+    @Test
+    public void throw_an_exception_when_the_username_has_not_the_right_token() {
+        UserToken userToken = new UserToken("wrongToken");
+
+        Assertions.assertThrows(IncorrectTokenException.class, () -> user.checkToken(userToken));
     }
 }
